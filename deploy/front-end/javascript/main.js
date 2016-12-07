46ff3e6dec533d8af1f5bd829bbe5c2fe68e74f6
Vitaliy Vinnichenko <vinnichenko.vitaliy@gmail.com>
2016-12-07 18:29:05 +0200

    Navigation menu

--- deploy/front-end/javascript/main.js
+++ deploy/front-end/javascript/main.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', function(){
   loadLoginPage();
 }, false);
+
+document.addEventListener('click', function(event){
+  var target = event.target,
+      menu = document.getElementById('menu');
+
+  while(target != document){
+    if(target.getAttribute('data-toggle-menu') || target == menu){
+      removeClass(menu, 'hide');
+      addClass(menu, 'show');
+      menu.style.left = 0;
+      return;
+    }
+    target = target.parentNode;
+  }
+  removeClass(menu, 'show');
+  addClass(menu, 'hide');
+  menu.style.left = '-81vw';
+}, false);
