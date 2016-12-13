6a17a6129054589d0c33ee9b493c88f5f7583200
romanvit13 <romanvit13@gmail.com>
2016-12-13 19:27:36 +0200

    initial version of AndroidGeolocateMEApp

---
+++ prototypes/front-end/androidapp/GeolocateMEMap/app/src/main/java/com/example/roman/geolocatememap/FriendsMenu.java
@@ -0,0 +1,18 @@
+package com.example.roman.geolocatememap;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Roman on 10-Dec-16.
+ */
+
+public class FriendsMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_menu);
+    }
+
+}
