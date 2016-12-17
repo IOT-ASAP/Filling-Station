3b08cc8055fafb526e91ca5d0972e76c0cb34f87
romanvit13 <romanvit13@gmail.com>
2016-12-17 14:58:56 +0200

    lkl

---
+++ prototypes/front-end/androidapp/GeolocateMEMap/app/src/main/java/com/example/roman/geolocatememap/lsfld.java
@@ -0,0 +1,26 @@
+package com.example.roman.geolocatememap;
+
+/**
+ * Created by Roman on 17-Dec-16.
+ */
+import com.loopj.android.http.*
+
+
+    public class TwitterRestClient {
+        private static final String BASE_URL = "https://api.twitter.com/1/";
+
+        private static AsyncHttpClient client = new AsyncHttpClient();
+
+        public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        private static String getAbsoluteUrl(String relativeUrl) {
+            return BASE_URL + relativeUrl;
+        }
+    }
+
