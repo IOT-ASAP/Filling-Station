978423ad34dbfbadcf7703d71c305aed5d3bc48d
romanvit13 <romanvit13@gmail.com>
2016-12-17 20:27:33 +0200

    Try 2

---
+++ deploy/front-end/Android/GeolocateMe/app/src/main/java/com/example/roman/geolocateme/SettingMenu.java
@@ -0,0 +1,19 @@
+package com.example.roman.geolocateme;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Roman on 11-Dec-16.
+ */
+
+public class SettingMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_menu);
+    }
+
+}
